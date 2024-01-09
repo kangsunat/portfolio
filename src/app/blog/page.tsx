28332cae46a4724b57d311b053ee01329be54924
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-09 17:07:50 +0700

    section blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <main className="container p-8">
+      {/* latest post */}
+      <section>latest post</section>
+      {/* contect list */}
+      <section>contenct list</section>
+      {/* other post */}
+      <section>other post</section>
+      {/* subscribe */}
+      <section>subscribe</section>
+    </main>
+  );
+}
